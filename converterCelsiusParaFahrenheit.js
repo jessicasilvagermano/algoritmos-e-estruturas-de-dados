030e45251510bb0d317539e68058f61ca8d562e5
Jéssica Silva <jessicasilv3@gmail.com>
2023-06-26 14:33:39 -0300

    Organizando os arquivos em pastas

--- converterCelsiusParaFahrenheit.js
+++
@@ -1,9 +0,0 @@
-
-//Converte temperatura em Celsius para Fahrenheit'
-
-function converteCelsiusToFahrenheit(temperaturaEmCelsius) {
-    let temperaturaEmFahrenheit = temperaturaEmCelsius * 9/5 + 32;
-    return temperaturaEmFahrenheit ;
-  }
-  
- console.log(converteCelsiusToFahrenheit(30)) ;
